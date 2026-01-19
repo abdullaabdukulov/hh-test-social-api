28a17a74f214be20309ee5446369b39ed6ad87bf
Abdulla <abduqulovabdulla3108@gmail.com>
2026-01-19 18:13:50 +0500

    users admin updated

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,38 +1,51 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 User = get_user_model()
 
 
 @admin.register(User)
-class UserModelAdmin(UserAdmin):
+class UserModelAdmin(DjangoUserAdmin):
     list_display = (
         "id",
         "email",
         "username",
         "full_name",
+        "is_verified",
         "is_active",
         "created_at",
     )
-    list_filter = ("is_staff", "is_superuser", "is_active")
+    list_filter = ("is_verified", "is_staff", "is_superuser", "is_active")
     search_fields = ("email", "username", "full_name", "id")
-    ordering = ("full_name",)
+    ordering = ("-created_at",)
+    readonly_fields = ("id", "created_at", "updated_at")
+
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (
-            "Personal info",
-            {
-                "fields": (
-                    "full_name",
-                    "email",
-                )
-            },
-        ),
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal info", {"fields": ("full_name",)}),
+        ("Status", {"fields": ("is_verified", "is_active")}),
         (
             "Permissions",
+            {"fields": ("is_staff", "is_superuser", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
             {
-                "fields": ("is_active", "is_staff", "is_superuser"),
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "full_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
             },
         ),
     )
